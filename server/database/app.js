4b0884fc5d7bc6972ad8bed979bf143aa9185843
Narcissus52 <andresfmp52@gmail.com>
2024-09-26 22:49:51 -0400

    new commit

--- server/database/app.js
+++ server/database/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const  cors = require('cors')
-const app = express()
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -93,7 +93,7 @@ try {
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
   const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  let new_id = documents[0]['id']+1;
 
   const review = new Reviews({
 		"id": new_id,
